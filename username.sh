f602ced9e0ab317dbd0b5e6df0bc2e3a78be20d1
Juliana Glodek <glodek@chapman.edu>
2020-11-09 10:43:18 -0800

    Adds username.sh

---
+++ username.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# username.sh
+# Juliana Glodek
+echo "Hello! Your username can only contain lower-case letters, digits, and the underscore character. Additionally, your username must start with a lower-case letter and contain 3-12 characters in total."
+echo "Enter a username: "
+read USERNAME
+while echo "$USERNAME" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+do
+	echo "You must enter a valid username - starting with a lower case letter, 3-12 characters, and only using lower case letters, digits, and underscore!!"
+	echo "Enter a valid username: "
+	read USERNAME
+done
+echo "Thank you"
